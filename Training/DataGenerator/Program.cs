d00208af8b53c0de936446e3a8755222637e44ec
Blealtan Cao <blealtan@gmail.com>
2017-07-01 14:18:18 +0800

    Project structure needed by training works.

---
+++ Training/DataGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meowtrix.osuAMT.Training.DataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
