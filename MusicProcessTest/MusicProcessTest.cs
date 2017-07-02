59fa33d83a82defc299c3dde542e03168dd0aa6b
Blealtan Cao <blealtan@gmail.com>
2017-07-02 22:45:22 +0800

    Change music processing test from XUnit Fact to Theory.

--- MusicProcessTest/MusicProcessTest.cs
+++ MusicProcessTest/MusicProcessTest.cs
@@ -10,13 +10,13 @@ namespace MusicProcessTest
 {
     public class MusicProcessTest
     {
-        [Fact]
-        public void Test()
+        [Theory]
+        [InlineData("Resource/440Hz-5sec.mp3")]
+        public void Test(string file)
         {
             byte[] mp3;
             using (var stream = new FileStream(Path.Combine(
-                Path.GetDirectoryName(typeof(MusicProcessTest).GetTypeInfo().Assembly.Location),
-                "Resource/440Hz-5sec.mp3"), FileMode.Open, FileAccess.Read))
+                Path.GetDirectoryName(typeof(MusicProcessTest).GetTypeInfo().Assembly.Location), file), FileMode.Open, FileAccess.Read))
             using (var reader = new BinaryReader(stream))
                 mp3 = reader.ReadBytes((int)stream.Length);
             var result = MusicProcess.ProcessMp3(mp3);
