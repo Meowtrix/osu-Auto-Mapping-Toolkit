7421be32a673f214a9e9566bcee3ce8a79b3d44e
Blealtan Cao <blealtan@gmail.com>
2017-07-02 17:01:16 +0800

    Mp3 decoding and array shaping in MusicProcess.

---
+++ MusicProcess/MusicProcess.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Meowtrix.osuAMT.MusicProcess
+{
+    using hip_t = IntPtr;
+
+    public static class MusicProcess
+    {
+        const string LAME = "libmp3lame";
+
+        [DllImport(LAME, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern hip_t hip_decode_init();
+
+        [DllImport(LAME, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int hip_decode_exit(hip_t gfp);
+
+        [DllImport(LAME, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern unsafe int hip_decode1(hip_t gfp, byte* buffer, [param: MarshalAs(UnmanagedType.SysInt)] int len, short* pcm_l, short* pcm_r);
+
+        /// <summary>
+        /// Process mp3 data for use in training and evaluating the machine learning models.
+        /// </summary>
+        /// <param name="mp3">Mp3 data in memory.</param>
+        /// <param name="fft">
+        ///     Optional; if provided, this method will use it for doing FFT, otherwise a managed version will be used.
+        ///     Parameters are data buffer and block size; provided optimized algorithm should do FFT block by block.
+        ///     block size is promised to be power of two.
+        /// </param>
+        /// <returns>Matrix as data for the ML models. One row for a data point in the sequence.</returns>
+        public static short[,] ProcessMp3(byte[] mp3, Action<short[], int> fft = null)
+        {
+            var leftFrames = new List<short[]>();
+            var leftBuff = new short[65536];
+            var rightFrames = new List<short[]>();
+            var rightBuff = new short[65536];
+            int totalLength = 0;
+            var gfp = hip_decode_init();
+
+            // Use LAME to decode the mp3 data.
+            unsafe
+            {
+                fixed (byte* input = mp3)
+                fixed (short* pcm_l = leftBuff)
+                fixed (short* pcm_r = rightBuff)
+                {
+                    int frameLength;
+                    while ((frameLength = hip_decode1(gfp, input, mp3.Length, pcm_l, pcm_r)) > 0)
+                    {
+                        var leftFrame = new short[frameLength];
+                        var rightFrame = new short[frameLength];
+                        Array.Copy(leftBuff, leftFrame, frameLength);
+                        Array.Copy(rightBuff, rightFrame, frameLength);
+                        leftFrames.Add(leftFrame);
+                        rightFrames.Add(rightFrame);
+                        totalLength += frameLength;
+                    }
+                    if (frameLength < 0) throw new Exception("MP3 file decoding failed.");
+                }
+            }
+
+            // Padding the size so that it's a multiple of 256.
+            var leftPCM = new short[(totalLength + 255) & ~255];
+            var rightPCM = new short[(totalLength + 255) & ~255];
+            leftFrames.Aggregate(0, (n, frame) => { Buffer.BlockCopy(frame, 0, leftPCM, n, frame.Length * sizeof(short)); return n + frame.Length; });
+            rightFrames.Aggregate(0, (n, frame) => { Buffer.BlockCopy(frame, 0, rightPCM, n, frame.Length * sizeof(short)); return n + frame.Length; });
+
+            // If no optimized FFT algorithm is provided, use managed version.
+            fft = fft ?? ManagedFFT;
+
+            // Execute FFT.
+            fft(leftPCM, 256);
+            fft(rightPCM, 256);
+
+            // Reshape the result into desired dimension.
+            var reshaped = new short[(totalLength + 255) / 256, 512];
+            for (int i = 0; i < (totalLength + 255) / 256; ++i)
+            {
+                Buffer.BlockCopy(leftPCM, i * 256, reshaped, i * 512, 256);
+                Buffer.BlockCopy(rightPCM, i * 256, reshaped, i * 512 + 256, 256);
+            }
+
+            return reshaped;
+        }
+
+        private static void ManagedFFT(short[] data, int blockSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
