bc99bed0388d2d5691915943e5307144e2ba953c
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-07-03 19:53:35 +0800

    Read file from osz archive.

---
+++ Training/DataGenerator/OszArchive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace Meowtrix.osuAMT.Training.DataGenerator
+{
+    class OszArchive : Archive, IDisposable
+    {
+        public ZipArchive archive;
+
+        public OszArchive(Stream stream)
+        {
+            archive = new ZipArchive(stream, ZipArchiveMode.Read, false);
+        }
+
+        public void Dispose() => archive.Dispose();
+
+        public override Stream OpenFile(string filename) => archive.GetEntry(filename).Open();
+
+        public override IEnumerable<Stream> OpenOsuFiles() => archive.Entries.Where(x => x.Name.EndsWith(".osz")).Select(e => e.Open());
+    }
+}
