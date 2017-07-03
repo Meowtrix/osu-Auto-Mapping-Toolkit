505be9b9e1e141e63ac613c9bb3f6ea6482cf0cc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-07-03 19:46:04 +0800

    Read file from folder storage.

---
+++ Training/DataGenerator/Archive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Meowtrix.osuAMT.Training.DataGenerator
+{
+    abstract class Archive
+    {
+        public abstract IEnumerable<Stream> OpenOsuFiles();
+        public abstract Stream OpenFile(string filename);
+    }
+}
---
+++ Training/DataGenerator/Folder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Meowtrix.osuAMT.Training.DataGenerator
+{
+    class Folder : Archive
+    {
+        private DirectoryInfo directory;
+
+        public Folder(DirectoryInfo directory)
+        {
+            this.directory = directory;
+        }
+
+        public override Stream OpenFile(string filename)
+            => File.OpenRead(Path.Combine(directory.FullName, filename));
+
+        public override IEnumerable<Stream> OpenOsuFiles()
+            => directory.EnumerateFiles("*.osu", SearchOption.AllDirectories).Select(f => f.OpenRead());
+    }
+}
