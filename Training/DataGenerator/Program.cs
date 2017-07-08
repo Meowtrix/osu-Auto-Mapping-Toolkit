22e27a9ce992f8d6526d1d61c8e2c9e842c54da7
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-07-08 10:27:53 +0800

    Generate training data.

--- Training/DataGenerator/Program.cs
+++ Training/DataGenerator/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
+using Meowtrix.osuAMT.MusicProcess;
 
 namespace Meowtrix.osuAMT.Training.DataGenerator
 {
@@ -44,6 +47,7 @@ namespace Meowtrix.osuAMT.Training.DataGenerator
             var committedSongs = 0;
 
             var reportTimer = new Timer((obj) => Console.WriteLine($"{committedSongs}/{songs.Count} songs committed for processing."), null, 0, 10000);
+            output = new BinaryWriter(File.OpenWrite("output.bin"));
 
             var workers = new Thread[parallel];
             using (var enumerator = songs.GetEnumerator())
@@ -68,13 +72,123 @@ namespace Meowtrix.osuAMT.Training.DataGenerator
 
             foreach (var thread in workers)
                 thread.Join();
+            output.Flush();
+            output.Dispose();
 
             return 0;
         }
 
+        static BinaryWriter output;
+
         static void ProcessData(Archive archive)
         {
+            var timingList = new List<(double time, double beatLength, int sectionLength, bool kiai)>();
+            string audioFile = null;
+            foreach (var stream in archive.OpenOsuFiles())
+                using (var reader = new StreamReader(stream))
+                {
+                    while (reader.ReadLine().Trim() != "[General]") ;
+                    string audio;
+                    while (true)
+                    {
+                        string[] split = reader.ReadLine().Split(':');
+                        if (split[0] == "AudioFilename")
+                        {
+                            audio = split[1].Trim();
+                            break;
+                        }
+                    }
+                    if (audioFile == null)
+                        audioFile = audio;
+                    else if (audioFile != audio)
+                        continue;
 
+                    while (reader.ReadLine().Trim() != "[TimingPoints]") ;
+                    string timing;
+                    while (!string.IsNullOrWhiteSpace(timing = reader.ReadLine()))
+                    {
+                        string[] split = timing.Split(',');
+                        double time = double.Parse(split[0]);
+                        double beatLength = double.Parse(split[1]);
+
+                        int sectionLength = 4;
+                        if (split.Length >= 3)
+                            sectionLength = split[2][0] == '0' ? 4 : int.Parse(split[2]);
+
+                        bool timingChange = true;
+                        if (split.Length >= 7)
+                            timingChange = split[6][0] == '1';
+
+                        bool kiai = false;
+                        if (split.Length >= 8)
+                            kiai = (int.Parse(split[7]) & 1) > 0;
+
+                        int i = 0;
+                        for (i = 0; i < timingList.Count; i++)
+                        {
+                            var point = timingList[i];
+                            if (point.time == time) i = -1;
+                            if (point.time >= time) break;
+                        }
+
+                        if (i == -1) continue;
+                        if (timingChange || i > 0 && (kiai != timingList[i - 1].kiai))
+                            timingList.Insert(i, (time, beatLength, sectionLength, kiai));
+                    }
+                }
+            if (audioFile?.EndsWith(".mp3") != true) return;
+            if (timingList.Count == 0) return;
+
+            float[,] fft;
+            using (var mp3 = archive.OpenFile(audioFile))
+                fft = MusicProcesser.ProcessMp3(mp3);
+
+            int sampleCount = fft.GetUpperBound(0) + 1;
+            byte[] data = new byte[sampleCount];
+            timingList.Add((double.MaxValue, double.NaN, 0, false)); //fence item
+
+            int beat = 0;
+            double length = double.MaxValue;
+            for (int i = 0; i < timingList.Count - 1; i++)
+            {
+                var thisPoint = timingList[i];
+                double nextTime = timingList[i + 1].time - 2; //as epsilon
+
+                if (thisPoint.beatLength > 0)
+                {
+                    beat = 0;
+                    length = thisPoint.beatLength;
+                }
+
+                for (double time = thisPoint.time; time < nextTime; time += length)
+                {
+                    int sample = (int)(time * 44.1 / 256);
+                    if (sample >= data.Length) break;
+
+                    byte signature = 1;
+                    if (beat % thisPoint.sectionLength == 0) signature |= 2;
+                    if (thisPoint.kiai) signature |= 4;
+
+                    data[sample] = signature;
+                    beat++;
+                }
+            }
+
+            byte[] fftbytes = new byte[fft.Length * sizeof(float)];
+            Buffer.BlockCopy(fft, 0, fftbytes, 0, fftbytes.Length);
+
+            var idstr = Regex.Match(archive.Name, @"\d+");
+            int id = -1;
+            if (idstr.Success)
+                id = int.Parse(idstr.Value);
+
+            lock (output)
+            {
+                output.Write(id);
+                output.Write(sampleCount);
+                output.Write(fftbytes);
+                output.Write(data);
+            }
         }
     }
 }
